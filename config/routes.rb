bae0c395222d600a893dcb58c4f347d875191593
min-0000 <bigdogdag13@gmail.com>
2019-12-08 15:16:23 +0900

    home#about追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'rooms/show'
   devise_for :users
   root to: "home#index"
-
+  get 'about',to:'home#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
